84db5f592eb74876f0c7e12babe2229e6bd71755
Quang lam <lamdieuquang0105@gmail.com>
2025-12-05 18:20:02 +0700

    thử nghiệm 2 lỗi cho github action mới

--- backend/src/main/java/com/bookstore/backend/controller/AuthController.java
+++ backend/src/main/java/com/bookstore/backend/controller/AuthController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 @CrossOrigin(origins = "*")
 public class AuthController {
 
-  private final AuthService authService;
+  private final AuthServices authService;
 
   public AuthController(AuthService authService) {
     this.authService = authService;
